6f4e31f0cdfa9ed42025a52b1b689aa278d85829
rawlingsj <rawlingsj80@gmail.com>
2016-08-15 23:18:32 +0100

    figure out what kind of exposer rule we need to work on the target kubernetes based implementation

--- exposer.go
+++ exposer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -12,6 +13,15 @@ import (
 	"k8s.io/kubernetes/pkg/controller/framework"
 	"k8s.io/kubernetes/pkg/runtime"
 	"k8s.io/kubernetes/pkg/watch"
+)
+
+const (
+	exposerRule        = "exposer-rule"
+	fabric8Environment = "fabric8-environment"
+	ingress            = "ingress"
+	loadBalancer       = "load-balancer"
+	nodePort           = "node-port"
+	route              = "route"
 )
 
 func main() {
@@ -32,7 +42,7 @@ func main() {
 		&api.Service{},
 		time.Millisecond*100,
 		framework.ResourceEventHandlerFuncs{
-			AddFunc:    serviceAdded,
+			AddFunc:    serviceAdded(c),
 			DeleteFunc: serviceDeleted,
 		},
 	)
@@ -43,13 +53,16 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func serviceAdded(obj interface{}) {
-	svc := obj.(*api.Service)
-	addExposerRule(svc.ObjectMeta.Name)
+func serviceAdded(c *client.Client) func(obj interface{}) {
+	return func(obj interface{}) {
+		svc := obj.(*api.Service)
+		addExposerRule(c, svc.ObjectMeta.Name, svc.Namespace)
+	}
 }
 
 func serviceDeleted(obj interface{}) {
 	svc, ok := obj.(cache.DeletedFinalStateUnknown)
+
 	if ok {
 		// service key is in the form namespace/name
 		deleteExposerRule(strings.Split(svc.Key, "/")[1])
@@ -63,9 +76,33 @@ func serviceDeleted(obj interface{}) {
 	}
 }
 
-func addExposerRule(svc string) {
-	log.Println("Service created: " + svc)
+func addExposerRule(c *client.Client, svc string, ns string) {
+	log.Println("Service created [" + svc + "] in namespace [" + ns + "]")
+	currentNs := os.Getenv("KUBERNETES_NAMESPACE")
+	if len(currentNs) <= 0 {
+		log.Fatalf("No KUBERNETES_NAMESPACE env var set")
+	}
+
+	environment, err := c.ConfigMaps(currentNs).Get(fabric8Environment)
+	if err != nil {
+		log.Fatalf("No ConfigMap with name [" + fabric8Environment + "] found in namespace [" + currentNs + "].  Was the exposer namespace setup by gofabric8?")
+	}
+
+	switch environment.Data[exposerRule] {
+	case ingress:
+		log.Println("Creating Ingress")
+	case route:
+		log.Println("Creating OpenShift Route")
+	case nodePort:
+		log.Println("Adapting Service type to be NodePort")
+	case loadBalancer:
+		log.Println("Adapting Service type to be LoadBalancer, this can take a few minutes to ve create by cloud provider")
+	default:
+		log.Fatalf("No match for [" + environment.Data[exposerRule] + "] exposer-rule found.  Was the exposer namespace setup by gofabric8?")
+	}
 }
+
 func deleteExposerRule(svc string) {
-	log.Println("Service deleted: " + svc)
+	// TODO how do we get the namespace of the deleted service so we can find the corresponding rule / route to delete?
+	log.Println("Service deleted [" + svc + "], not deleting the exposer rule yet until we figure out how to get the namespace from a DeletedFinalStateUnknown service object")
 }
