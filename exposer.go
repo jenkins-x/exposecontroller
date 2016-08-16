d28f8781e7b0b007c7b1af9486fa6e0cad7a48a4
rawlingsj <rawlingsj80@gmail.com>
2016-08-16 10:00:07 +0100

    generate ingress rules

--- exposer.go
+++ exposer.go
@@ -7,7 +7,11 @@ import (
 	"strings"
 	"time"
 
+	rapi "github.com/openshift/origin/pkg/route/api"
+	rapiv1 "github.com/openshift/origin/pkg/route/api/v1"
 	"k8s.io/kubernetes/pkg/api"
+	kapi "k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/extensions"
 	"k8s.io/kubernetes/pkg/client/cache"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
 	"k8s.io/kubernetes/pkg/controller/framework"
@@ -16,6 +20,7 @@ import (
 )
 
 const (
+	domain             = "domain"
 	exposerRule        = "exposer-rule"
 	fabric8Environment = "fabric8-environment"
 	ingress            = "ingress"
@@ -56,7 +61,7 @@ func main() {
 func serviceAdded(c *client.Client) func(obj interface{}) {
 	return func(obj interface{}) {
 		svc := obj.(*api.Service)
-		addExposerRule(c, svc.ObjectMeta.Name, svc.Namespace)
+		addExposerRule(c, svc, svc.Namespace)
 	}
 }
 
@@ -65,7 +70,7 @@ func serviceDeleted(obj interface{}) {
 
 	if ok {
 		// service key is in the form namespace/name
-		deleteExposerRule(strings.Split(svc.Key, "/")[1])
+		deleteExposerRule(svc.Key)
 	} else {
 		svc, ok := obj.(*api.Service)
 		if ok {
@@ -76,8 +81,8 @@ func serviceDeleted(obj interface{}) {
 	}
 }
 
-func addExposerRule(c *client.Client, svc string, ns string) {
-	log.Println("Service created [" + svc + "] in namespace [" + ns + "]")
+func addExposerRule(c *client.Client, svc *api.Service, ns string) {
+	log.Println("Service created [" + svc.ObjectMeta.Name + "] in namespace [" + ns + "]")
 	currentNs := os.Getenv("KUBERNETES_NAMESPACE")
 	if len(currentNs) <= 0 {
 		log.Fatalf("No KUBERNETES_NAMESPACE env var set")
@@ -88,8 +93,17 @@ func addExposerRule(c *client.Client, svc string, ns string) {
 		log.Fatalf("No ConfigMap with name [" + fabric8Environment + "] found in namespace [" + currentNs + "].  Was the exposer namespace setup by gofabric8?")
 	}
 
+	d, ok := environment.Data[domain]
+	if !ok {
+		log.Fatalf("No ConfigMap data with name [" + domain + "] found in namespace [" + currentNs + "].  Was the exposer namespace setup by gofabric8?")
+	}
+
 	switch environment.Data[exposerRule] {
 	case ingress:
+		err := createIngress(ns, d, svc, c)
+		if err != nil {
+			log.Fatalf("Unable to create ingress rule for service " + svc.ObjectMeta.Name)
+		}
 		log.Println("Creating Ingress")
 	case route:
 		log.Println("Creating OpenShift Route")
@@ -103,6 +117,96 @@ func addExposerRule(c *client.Client, svc string, ns string) {
 }
 
 func deleteExposerRule(svc string) {
-	// TODO how do we get the namespace of the deleted service so we can find the corresponding rule / route to delete?
-	log.Println("Service deleted [" + svc + "], not deleting the exposer rule yet until we figure out how to get the namespace from a DeletedFinalStateUnknown service object")
+
+	ns := strings.Split(svc, "/")[0]
+	name := strings.Split(svc, "/")[1]
+
+	log.Println("Service deleted [" + name + "] in namespace [" + ns + "]")
+}
+
+func createIngress(ns string, domain string, service *api.Service, c *client.Client) error {
+	rapi.AddToScheme(kapi.Scheme)
+	rapiv1.AddToScheme(kapi.Scheme)
+
+	ingressClient := c.Extensions().Ingress(ns)
+	ingresses, err := ingressClient.List(kapi.ListOptions{})
+	if err != nil {
+		log.Printf("Failed to load ingresses in namespace %s with error %v", ns, err)
+		return err
+	}
+
+	var labels = make(map[string]string)
+	labels["provider"] = "fabric8"
+
+	name := service.ObjectMeta.Name
+	serviceSpec := service.Spec
+
+	found := false
+
+	// for now lets use the type of the service to know if we should create an ingress
+	// TODO we should probably add an annotation to disable ingress creation
+	if name != "jenkinshift" {
+		for _, ingress := range ingresses.Items {
+			if ingress.GetName() == name {
+				found = true
+				break
+			}
+			// TODO look for other ingresses with different names?
+			for _, rule := range ingress.Spec.Rules {
+				http := rule.HTTP
+				if http != nil {
+					for _, path := range http.Paths {
+						ruleService := path.Backend.ServiceName
+						if ruleService == name {
+							found = true
+							break
+						}
+					}
+				}
+			}
+		}
+		if !found {
+			ports := serviceSpec.Ports
+			hostName := name + "." + ns + "." + domain
+			if len(ports) > 0 {
+				rules := []extensions.IngressRule{}
+				for _, port := range ports {
+					rule := extensions.IngressRule{
+						Host: hostName,
+						IngressRuleValue: extensions.IngressRuleValue{
+							HTTP: &extensions.HTTPIngressRuleValue{
+								Paths: []extensions.HTTPIngressPath{
+									{
+										Backend: extensions.IngressBackend{
+											ServiceName: name,
+											// we need to use target port until https://github.com/nginxinc/kubernetes-ingress/issues/41 is fixed
+											//ServicePort: intstr.FromInt(port.Port),
+											ServicePort: port.TargetPort,
+										},
+									},
+								},
+							},
+						},
+					}
+					rules = append(rules, rule)
+				}
+				ingress := extensions.Ingress{
+					ObjectMeta: kapi.ObjectMeta{
+						Labels: labels,
+						Name:   name,
+					},
+					Spec: extensions.IngressSpec{
+						Rules: rules,
+					},
+				}
+				// lets create the ingress
+				_, err = ingressClient.Create(&ingress)
+				if err != nil {
+					log.Printf("Failed to create the ingress %s with error %v", name, err)
+					return err
+				}
+			}
+		}
+	}
+	return nil
 }
