202b4653bb1eb147eb4e74d8ea1fe00961a00947
rawlingsj <rawlingsj80@gmail.com>
2016-08-19 12:38:05 +0100

    nodeport does work on minishift after all!  oc export doesnt show it though which mislead us to think it wasnt working

--- exposecontroller.go
+++ exposecontroller.go
@@ -199,13 +199,7 @@ func addExposeRule(c *kclient.Client, oc *osclient.Client, svc *api.Service, cur
 			createRoute(svc.Namespace, d, svc, c, oc)
 		}
 	case nodePort:
-		// NodePort doesnt seem to work on openshift
-		if util.TypeOfMaster(c) == util.OpenShift {
-			util.Warn("Switching to use LoadBalancer type as NodePort not working on MiniShift")
-			useLoadBalancer(svc.Namespace, svc, c)
-		} else {
-			useNodePort(svc.Namespace, svc, c)
-		}
+		useNodePort(svc.Namespace, svc, c)
 
 	case loadBalancer:
 		useLoadBalancer(svc.Namespace, svc, c)
